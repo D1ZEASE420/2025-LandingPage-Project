4a867266ad802e484ce8590ea10e3986f7a328a3
Force <marcusmesila243@gmail.com>
2025-11-06 15:15:55 +0000

    Update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -25,6 +25,23 @@ function App() {
         <p class="text-xl font-semibold pb-5">JOIN GAME</p>
       <button class="flex items-center justify-center rounded-xl w-60 text-2xl p-5 shadow-lg border-1 border-neutral-200 font-semibold">ENTER PIN</button>
     </div>
+    <div>
+      <h1 class="text-4xl font-semibold pt-155 text-center">Choose</h1>
+      <p className="mt-4 text-center text-2xl font-medium pb-100">where you play in the <span className="text-blue-600">real world</span></p>
+    </div>
+    </div>
+    <div class="w-100 h-100 border-1 border-neutral-200 rounded-2xl flex justify-center flex-col items-center shadow-lg">
+    </div>
+    <div>
+      <p class="text-2xl font-semibold flex justify-center text-center pt-5">Coming soon</p>
+    </div>
+    <div class="flex justify-center">
+      <button class="flex justify-center rounded-xl text-2xl p-3 m-2 font-semibold shadow-lg border-1 border-neutral-200 h-15 w-15">25</button>
+      <button class="flex justify-center rounded-xl text-2xl p-3 m-2 font-semibold shadow-lg border-1 border-neutral-200 h-15 w-15">10</button>
+      <button class="flex justify-center rounded-xl text-2xl p-3 m-2 font-semibold shadow-lg border-1 border-neutral-200 h-15 w-15">52</button>
+    </div>
+    <div class="pt-10">
+      <p class="">Contacts:</p>
     </div>
     </>
   )
